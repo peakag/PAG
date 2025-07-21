c249b123d8d43e82d69db65daccc7592b9fa060e
peakag <kevinbites@outlook.com>
2025-07-21 12:19:26 -0400

    fix: remove invalid supabaseUrl and supabaseKey from middleware client init

--- middleware.ts
+++ middleware.ts
@@ -7,9 +7,7 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient<Database>({
     req,
-    res,
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    res
   });
   const {
     data: { session },
